2138f4d8b25c39f035fa4bc37eaebc267864b986
Kevin Aguilar <42625300+kelvinal1@users.noreply.github.com>
2021-09-29 11:04:59 -0500

    Add files via upload

---
+++ Proyecto_titulacion/src/test/java/m4a/Proyecto_titulacion/ProyectoTitulacionApplicationTests.java
@@ -0,0 +1,13 @@
+package m4a.Proyecto_titulacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTitulacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
